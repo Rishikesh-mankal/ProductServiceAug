da5b7d1bd26586dda1edad16081612307cc274be
Rishi <rishikeshmankal9@gmail.com>
2024-08-17 20:35:19 +0530

    added configurations

--- src/main/java/com/rishi/productservice15aug/service/FakeStoreService.java
+++ src/main/java/com/rishi/productservice15aug/service/FakeStoreService.java
@@ -2,10 +2,16 @@ package com.rishi.productservice15aug.service;
 
 import com.rishi.productservice15aug.model.Product;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class FakeStoreService implements ProductService {
 
+    RestTemplate restTemplate;
+
+    public FakeStoreService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     @Override
     public Product getProductById(Long id) {
